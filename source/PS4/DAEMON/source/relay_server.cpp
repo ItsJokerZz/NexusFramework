5d3190563608bf0546a1b4923249aca8eb464857
ItsJokerZz <ItsJokerZz@Gmail.com>
2025-01-19 13:02:46 -0500

    pt2

--- source/PS4/DAEMON/source/relay_server.cpp
+++ source/PS4/DAEMON/source/relay_server.cpp
@@ -36,7 +36,7 @@ namespace server
                     {"GET /ping", cmds::daemon::ping_relay},
                     {"GET /attach", cmds::daemon::attach_relay},
                     {"GET /exec_prx", cmds::daemon::load_module},
-                    {"GET /load_plugin", cmds::daemon::start_plugin}};
+                    {"GET /load_plugin", cmds::daemon::start_plugin},
                     {"GET /rw_memory", cmds::daemon::rw_proc_mem}};
 
                 typedef std::map<std::string, std::function<void()>>::const_iterator CommandIter;
