65b318c248d67386bf908f42899892e5da7b7fd6
ItsJokerZz <ItsJokerZz@Gmail.com>
2025-01-20 01:03:56 -0500

    simplify

--- source/PS4/DAEMON/source/definitions.cpp
+++ source/PS4/DAEMON/source/definitions.cpp
@@ -9,18 +9,16 @@ std::array<ErrorMessage, ERROR_COUNT> error_messages = {{
     {"Invalid parameters provided. Please verify and retry."},
 }};
 
-pthread_t daemon_thread = -1, relay_thread = -1;
-
 bool isDaemon = false, unloaded = false,
      connected = false, attached = false;
 
 extern "C" int32_t __wrap__init(size_t args, const void *argp)
 {
+    pthread_t thread = -1;
     struct proc_info info;
     sys_sdk_proc_info(&info);
 
     isDaemon = (strcmp(info.titleid, DAEMON) == 0);
-    pthread_t *thread = isDaemon ? &daemon_thread : &relay_thread;
     void *(*thread_func)(void *) = isDaemon
                                        ? (void *(*)(void *))server::daemon::thread
                                        : (void *(*)(void *))server::relay::thread;
@@ -29,9 +27,9 @@ extern "C" int32_t __wrap__init(size_t args, const void *argp)
                                  (isDaemon ? "Daemon server started!" : "Relay server started"))
                                     .c_str());
 
-    if (*thread == -1)
+    if (thread == -1)
     {
-        if (pthread_create(thread, nullptr, thread_func, nullptr) != 0)
+        if (pthread_create(&thread, nullptr, thread_func, nullptr) != 0)
         {
             sys_utils::text_notify(222, (std::string("[OCAPI] Failed to create ") +
                                          (isDaemon ? "daemon" : "relay") + " thread!")
@@ -40,7 +38,7 @@ extern "C" int32_t __wrap__init(size_t args, const void *argp)
             return 1;
         }
 
-        pthread_detach(*thread);
+        pthread_detach(thread);
     }
 
     if (isDaemon)
