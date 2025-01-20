5f11c0a143e0224fd757eeee24814a79a1d0bc7e
ItsJokerZz <ItsJokerZz@Gmail.com>
2025-01-20 00:53:32 -0500

    more clean up

--- source/PS4/PAYLOAD/source/main.c
+++ source/PS4/PAYLOAD/source/main.c
@@ -1,24 +1,21 @@
 #include <ps4.h>
 
-static int32_t (*__wrap__init)(size_t, const void *);
+static int32_t (*__init)(size_t, const void *);
 
-int64_t sceKernelDlsym(int64_t moduleHandle, const char *functionName, void *destFuncOffset) {
+int64_t dlsym(int64_t moduleHandle, const char *functionName, void *destFuncOffset) {
   return (int64_t)syscall(591, (void *)moduleHandle, (void *)functionName, destFuncOffset);
 }
 
 int _main(void) {
-  initKernel();
-  initLibc();
-
   int prx_id;
+  
+  initKernel();
 
-  if (loadModule("/user/data/GoldHEN/plugins/OrbisControl.prx", &prx_id) != 0)
+  if (loadModule("/user/data/GoldHEN/plugins/OrbisControl.prx", &prx_id) != 0 ||
+      dlsym(prx_id, "__wrap__init", (void **)&__init) < 0 || __init == NULL)
     return -1;
 
-  if (sceKernelDlsym(prx_id, "__wrap__init", (void **)&__wrap__init) < 0 || __wrap__init == NULL)
-    return -1;
-
-  __wrap__init(0, NULL);
+  __init(0, NULL);
 
   return 0;
 }
